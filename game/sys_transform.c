02cd565e49175ea862758c99be19aecf8ce2596d
Staś Małolepszy <stas@piesku.com>
2020-11-22 23:40:59 +0100

    Allow children before parent in sys_transform

--- game/sys_transform.c
+++ game/sys_transform.c
@@ -7,43 +7,39 @@
 
 static int32_t QUERY = HAS_TRANSFORM;
 
-static inline void set_children_as_dirty(struct world* world, Transform* parent)
+static inline void update_transform(struct world* world, Transform* transform)
 {
-	for (int i = 0; i < MAX_CHILDREN; i++) {
-		entity child = parent->children[i];
-		if (child) {
-			Transform* transform = world->transform[child];
-			transform->dirty = true;
-			set_children_as_dirty(world, transform);
-		}
-	}
-}
-
-static inline void update(struct world* world, entity entity)
-{
-	Transform* transform = world->transform[entity];
-	if (transform->dirty) {
-		set_children_as_dirty(world, transform);
 		transform->dirty = false;
 
 		mat4_compose(transform->world,
 				transform->translation,
 				transform->rotation,
 				transform->scale);
+
 		if (transform->parent) {
 			mat4_multiply(transform->world,
 					world->transform[transform->parent]->world,
 					transform->world);
 		}
+
 		mat4_invert(transform->self, transform->world);
-	}
+
+		for (int i = 0; i < MAX_CHILDREN; i++) {
+			entity child = transform->children[i];
+			if (child) {
+				update_transform(world, world->transform[child]);
+			}
+		}
 }
 
 void sys_transform(struct client* client, struct world* world, float delta)
 {
 	for (entity i = 1; i < MAX_ENTITIES; i++) {
 		if ((world->signature[i] & QUERY) == QUERY) {
-			update(world, i);
+			Transform* transform = world->transform[i];
+			if (transform->dirty) {
+				update_transform(world, transform);
+			}
 		}
 	}
 }
