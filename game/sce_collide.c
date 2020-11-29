1ef185405c17fde4bbd5a6a51bbbd460d51bcaba
Staś Małolepszy <stas@piesku.com>
2020-11-29 02:59:39 +0100

    (sce_collide) 500 dynamic cubes

--- game/sce_collide.c
+++ game/sce_collide.c
@@ -24,13 +24,13 @@ void scene_collide(struct world* world)
 		collide->mask = LAYER_TERRAIN;
 	}
 
-	for (int i = 0; i < 200; i++) {
+	for (int i = 0; i < 500; i++) {
 		entity entity = create_entity(world);
 
 		Transform* transform = mix_transform(world, entity);
-		transform->translation[0] = rand() % 10 - 5;
-		transform->translation[1] = rand() % 10 - 5;
-		transform->translation[2] = rand() % 10 - 5;
+		transform->translation[0] = (rand() % 10 - 5) * 0.9;
+		transform->translation[1] = (rand() % 10 - 5) * 0.9;
+		transform->translation[2] = (rand() % 10 - 5) * 0.9;
 
 		RenderColoredUnlit* render = mix_render_colored_unlit(world, entity);
 		render->material = MAT_COLORED_UNLIT;
@@ -40,7 +40,7 @@ void scene_collide(struct world* world)
 		render->color[2] = (rand() % 100) / 100.0f;
 
 		Collide* collide = mix_collide(world, entity);
-		collide->dynamic = false;
+		collide->dynamic = true;
 		collide->layers = LAYER_TERRAIN;
 		collide->mask = LAYER_TERRAIN | LAYER_PLAYER;
 	}
