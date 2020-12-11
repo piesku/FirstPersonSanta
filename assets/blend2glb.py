3928db0285728f228f5292a06d439ef4232d2b3c
Michał Budzyński <michal@virtualdesign.pl>
2020-12-11 23:31:06 +0100

    blend to glb makefile

---
+++ assets/blend2glb.py
@@ -0,0 +1,16 @@
+import bpy
+import sys
+
+# get all args after "--"
+argv = sys.argv[sys.argv.index("--") + 1:]
+
+# join all meshes
+bpy.ops.object.select_all(action="DESELECT")
+bpy.ops.object.select_by_type(type="MESH")
+bpy.ops.object.join()
+
+# export obj
+bpy.ops.export_scene.gltf(
+    filepath=argv[0],
+    export_format='GLB',
+    check_existing=False)
