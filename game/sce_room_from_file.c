4470152daec83c60db74eb40eec89d96a4b4eedc
Staś Małolepszy <stas@piesku.com>
2020-12-11 23:47:38 +0100

    blueprint_ground

--- game/sce_room_from_file.c
+++ game/sce_room_from_file.c
@@ -16,6 +16,7 @@ void load_scene_from_gltf(struct world* world, const char* file_location);
 
 entity blueprint_camera_follow(struct world* world);
 entity blueprint_player_target(struct world* world, entity* target);
+entity blueprint_ground(struct world* world);
 
 void scene_room_from_file(struct world* world)
 {
@@ -24,42 +25,21 @@ void scene_room_from_file(struct world* world)
 		// Player.
 		entity target;
 		entity player = blueprint_player_target(world, &target);
-		Transform* transform = world->transform[player];
-		transform->translation = (vec3){0.0f, 1.7f, 5.0f};
+		Transform* player_transform = world->transform[player];
+		player_transform->translation = (vec3){0.0f, 1.7f, 5.0f};
 
-		{
-			entity camera = blueprint_camera_follow(world);
-			Transform* camera_transform = world->transform[camera];
-			camera_transform->translation = (vec3){0.0f, 50.0f, 1000.0f};
-			quat_from_euler(&camera_transform->rotation, 30.0f, 180.0f, 0.0f);
+		entity camera = blueprint_camera_follow(world);
+		Transform* camera_transform = world->transform[camera];
+		camera_transform->translation = (vec3){0.0f, 50.0f, 1000.0f};
+		quat_from_euler(&camera_transform->rotation, 30.0f, 180.0f, 0.0f);
 
-			Mimic* mimic = world->mimic[camera];
-			mimic->target = target;
-			mimic->stiffness = 0.1f;
-		}
+		Mimic* camera_mimic = world->mimic[camera];
+		camera_mimic->target = target;
+		camera_mimic->stiffness = 0.1f;
 	}
 
 	{
-		// Ground
-		entity entity = create_entity(world);
-
-		Transform* transform = mix_transform(world, entity);
-		transform->translation.y = -50.0f;
-		transform->scale = (vec3){100.0f, 100.0f, 100.0f};
-
-		RenderColoredUnlit* render = mix_render_colored_unlit(world, entity);
-		render->material = MAT_COLORED_UNLIT;
-		render->mesh = MESH_CUBE;
-		render->color = (vec4){0.32f, 0.4f, 0.88f, 1.0f};
-
-		Collide* collide = mix_collide(world, entity);
-		collide->dynamic = false;
-		collide->layers = LAYER_TERRAIN;
-		collide->mask = LAYER_NONE;
-		collide->aabb.size = (vec3){100.0f, 100.0f, 100.0f};
-
-		RigidBody* rigid_body = mix_rigid_body(world, entity);
-		rigid_body->kind = RIGID_STATIC;
+		blueprint_ground(world);
 	}
 
 	load_scene_from_gltf(world, "scenes/scene2.gltf");
