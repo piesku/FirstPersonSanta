e1d7c844c23e41d889f2d413800c6531ab6c2a35
Michał Budzyński <michal@virtualdesign.pl>
2020-12-13 18:13:09 +0100

    2 new rooms

--- game/sce_room_from_file.c
+++ game/sce_room_from_file.c
@@ -45,12 +45,12 @@ void scene_room_from_file(struct world* world)
 	Entity first = load_scene_from_gltf(world, "scenes/room01.gltf");
 	(void)first;
 
-	Entity second = load_scene_from_gltf(world, "scenes/room02.gltf");
+	Entity second = load_scene_from_gltf(world, "scenes/room06.gltf");
 	quat_from_euler(&world->transform[second]->rotation, 0, 90, 0);
 
 	Entity third = load_scene_from_gltf(world, "scenes/room03.gltf");
 	quat_from_euler(&world->transform[third]->rotation, 0, 180, 0);
 
-	Entity fourth = load_scene_from_gltf(world, "scenes/room04.gltf");
+	Entity fourth = load_scene_from_gltf(world, "scenes/room05.gltf");
 	quat_from_euler(&world->transform[fourth]->rotation, 0, 270, 0);
 }
